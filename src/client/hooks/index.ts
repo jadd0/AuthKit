e1516609f75ee5b4e5979484fa56830711d3080a
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-12-01 16:59:52 +0000

    Created a hook to retrieve session state

---
+++ src/client/hooks/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @module client/hooks
+ * @description React hooks for AuthKit client-side authentication.
+ */
+
+export * from "./useAuth";
--- src/client/hooks/useAuth.ts
+++ src/client/hooks/useAuth.ts
@@ -1,9 +1,8 @@
 import { useState, useEffect, use } from "react";
 import { ClientSession } from "../auth/clientSession";
-import { set } from "zod";
-import { Session } from "@/db/schemas";
 import { SessionWithUser } from "@/shared/types";
 
+/** React hook to manage and provide authentication state */
 export function useAuth() {
   const [session, setSession] = useState<SessionWithUser | null>(null);
   const [loading, setLoading] = useState(true);
