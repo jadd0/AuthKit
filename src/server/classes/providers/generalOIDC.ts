6e7e26d2b67edbb3895a2e86daaf47d315a14d8e
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-12-22 22:30:47 +0000

    Created a general OIDC class and a google implementaion of it

--- src/server/classes/providers/generalOIDC.ts
+++ src/server/classes/providers/generalOIDC.ts
@@ -1,30 +1,29 @@
-import { GeneralOIDC } from "./generalOIDC";
 
-export class GoogleProvider {
+/**
+ * @class GeneralOIDC
+ * @description This class provides a general OIDC provider creation, for ease of creating custom providers
+ */
+export class GeneralOIDC {
+  id: string;
+  issuer: string;
   clientId: string;
   clientSecret: string;
   redirectURI: string;
   scopes: string[];
-  generalOIDC: GeneralOIDC;
 
   constructor(
+    id: string,
+    issuer: string,
     clientId: string,
     clientSecret: string,
     redirectURI: string,
     scopes: string[]
   ) {
+    this.id = id;
+    this.issuer = issuer;
     this.clientId = clientId;
     this.clientSecret = clientSecret;
     this.redirectURI = redirectURI;
     this.scopes = scopes;
-
-    this.generalOIDC = new GeneralOIDC(
-      "google",
-      "https://accounts.google.com",
-      clientId,
-      clientSecret,
-      redirectURI,
-      scopes
-    );
   }
 }
