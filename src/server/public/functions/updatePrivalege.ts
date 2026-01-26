e95ff111ba65e54b143bee93362f6f6d8c448d5a
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2026-01-26 19:20:14 +0000

    Before updating user roles, now checks and validates against authConfig.roles

--- src/server/public/functions/updatePrivalege.ts
+++ src/server/public/functions/updatePrivalege.ts
@@ -2,6 +2,7 @@ import { DatabaseUserInteractions } from "@/server/db/interfaces/databaseUserInt
 import { auth as getAuth } from "./auth";
 import { auth } from "@/server/core/singleton";
 import { SessionWithUser } from "@/shared/types";
+import { authConfig } from "@/server/core/singleton";
 
 /**
  * Function to update user privilege
@@ -17,11 +18,25 @@ export async function updatePrivilege(
     throw new Error("No active session found.");
   }
 
+  // Check that the user roles are valid
+  const validRoles = authConfig.options.roles;
+  if (validRoles) {
+    for (const role of roles) {
+      if (!validRoles.includes(role)) {
+        throw new Error(`Invalid role: ${role}`);
+      }
+    }
+  }
+
   // Update the user's role in the database
   const updatedUser = await DatabaseUserInteractions.updateUserRole(
     roles,
     session.user.id,
   );
+
+  if (!updatedUser) {
+    throw new Error("Failed to update user roles.");
+  }
 
   // Rotate the session to get updated privileges
   const updatedSession = await auth.sessions.rotateSession(session.id);
